da17e6d6f4f4dd1910b4169b744f9cccda88fdfd
Jolenaz <jonasbellessa@gmail.com>
2017-03-19 19:49:01 +0100

    presque fini

---
+++ PHP/ex00/Colleen.php
@@ -0,0 +1,11 @@
+<?
+/*
+	com1
+*/
+function a(){$s='<?%3$c%5$c*%3$c%2$ccom1%3$c*%5$c%3$cfunction a(){$s=%4$c%1$s%4$c;printf($s,$s,9,10,39,47);}%3$cfunction main(){%3$c%5$c*%3$c%2$ccom2%3$c*%5$c%3$ca();}%3$cmain();';printf($s,$s,9,10,39,47);}
+function main(){
+/*
+	com2
+*/
+a();}
+main();
---
+++ PHP/ex01/Grace.php
@@ -0,0 +1,8 @@
+<?
+function main(){
+$s='<?%3$cfunction main(){%3$c$s=%4$c%1$s%4$c;%3$cprintf($s,$s,9,10,39,47);%3$c%5$c*%3$c%2$ccom%3$c*%5$c%3$}%3$c}main();';
+printf($s,$s,9,10,39,47);
+/*
+	com
+*/
+}main();
